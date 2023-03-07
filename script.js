bb11eb80c5058fc535407b15aa9a39e97bfabb56
Thiago Ferreira <thiagodnf@gmail.com>
2023-03-06 21:04:46 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+   $("#dog-button").click(function () {
+      $("#content").text("Woof Woof Woof");
+   })
+
+   $("#cat-button").click(function () {
+      $("#content").text("Meow Meow Meow");
+   });
+
+});
