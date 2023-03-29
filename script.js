b1c8d35e8a2ef06ed8717f1089dbea71207b294f
Cameron B <55037407+RusskiyMedved@users.noreply.github.com>
2023-03-29 15:41:25 -0400

    lab12

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ $(document).ready(function () {
 
    $("#dog-button").click(function () {
       $("#content").text("Woof Woof Woof");
+      $("#cat").hide();
+      $("#dog").show();
    })
 
    $("#cat-button").click(function () {
       $("#content").text("Meow Meow Meow");
+      $("#cat").show();
+      $("#dog").hide();
    });
 
 });
